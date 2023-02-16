ab0bfe23c2d2d4e82fe87be30b78afad9c5abbbe
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-02-16 15:44:38 +0800

    RAILS-NEXT-2 [BE] Created test case for auth an file upload

--- api/test/controllers/users_controller_test.rb
+++ api/test/controllers/users_controller_test.rb
@@ -3,8 +3,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'should get upload_avatar' do
-    get users_upload_avatar_url
-    assert_response :success
+  test 'should be able to upload_avatar' do
+    user_upload_avatar_params = {
+      user_id: 1,
+      avatar: fixture_file_upload('test.png', 'image/png')
+    }
+
+    post users_upload_avatar_url, params: user_upload_avatar_params, headers: token_headers
+    assert_response :ok
   end
 end
